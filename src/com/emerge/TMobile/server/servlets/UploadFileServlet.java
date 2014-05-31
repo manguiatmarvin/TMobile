855fd6764a26e88b3c5406661c4d0bd0929ef519
marvin <marvin@marvin.carrierinsite.com>
2014-06-01 02:43:21 +0800

    build file updated

--- src/com/emerge/TMobile/server/servlets/UploadFileServlet.java
+++ src/com/emerge/TMobile/server/servlets/UploadFileServlet.java
@@ -1,65 +1,111 @@
 package com.emerge.TMobile.server.servlets;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class UploadFileServlet extends HttpServlet{
-
-private long FILE_SIZE_LIMIT = 200 * 1024 * 1024; // 200 MiB
-private final Logger logger = Logger.getLogger("UploadServlet");
-
-protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-  throws ServletException, IOException {
-
-  try {
-    DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
-    ServletFileUpload fileUpload = new ServletFileUpload(fileItemFactory);
-    fileUpload.setSizeMax(FILE_SIZE_LIMIT);
-
-    List<FileItem> items = fileUpload.parseRequest(req);
-
-    for (FileItem item : items) {
-      if (item.isFormField()) {
-        logger.log(Level.INFO, "Received form field:");
-        logger.log(Level.INFO, "Name: " + item.getFieldName());
-        logger.log(Level.INFO, "Value: " + item.getString());
-      } else {
-        logger.log(Level.INFO, "Received file:");
-        logger.log(Level.INFO, "Name: " + item.getName());
-        logger.log(Level.INFO, "Size: " + item.getSize());
-      }
-
-      if (!item.isFormField()) {
-        if (item.getSize() > FILE_SIZE_LIMIT) {
-          resp.sendError(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE,
-            "File size exceeds limit");
-
-          return;
-        }
-
-        // Typically here you would process the file in some way:
-        // InputStream in = item.getInputStream();
-        // ...
-
-        if (!item.isInMemory())
-          item.delete();
-      }
+	private static final long serialVersionUID = 1L;
+    private static final String TMP_DIR_PATH = "/tmp";
+    private File tmpDir;
+    private static final String DESTINATION_DIR_PATH ="/home/marvin/Documents/myTemp";
+    private File destinationDir;
+    
+    public UploadFileServlet() {
+ 
     }
-  } catch (Exception e) {
-    logger.log(Level.SEVERE, "Throwing servlet exception for unhandled exception", e);
-    throw new ServletException(e);
-  }
-}
+    
 
-}  
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    	File file = new File("/home/marvin/Documents/TMobile/vis.xlsx");
+    	processExcelFile(file);
+    	
+    }
+    
+    
+    
+    
+    private int processExcelFile(File file){
+    	 
+    	 
+        int count = 0;
+ 
+        try{
+           
+            // Create a workbook using the File System 
+            Workbook myWorkBook  = WorkbookFactory.create(file);
+ 
+            // Get the first sheet from workbook 
+            Sheet mySheet = myWorkBook.getSheetAt(0);
+ 
+            /** We now need something to iterate through the cells.**/
+            Iterator<Row> rowIter = mySheet.rowIterator();
+            while(rowIter.hasNext()){
+ 
+                XSSFRow myRow = (XSSFRow) rowIter.next();
+                Iterator<Cell> cellIter = myRow.cellIterator();
+                while(cellIter.hasNext()){
+ 
+                    XSSFCell myCell = (XSSFCell) cellIter.next();
+                    //get cell index
+                    System.out.println("Cell column index: " + myCell.getColumnIndex());
+                    //get cell type
+                    System.out.println("Cell Type: " + myCell.getCellType());
+ 
+                    //get cell value
+                    switch (myCell.getCellType()) {
+                    case XSSFCell.CELL_TYPE_NUMERIC :
+                        System.out.println("Cell Value: " + myCell.getNumericCellValue());
+                        break;
+                    case XSSFCell.CELL_TYPE_STRING:   
+                        System.out.println("Cell Value: " + myCell.getStringCellValue());
+                        break;
+                    default:   
+                        System.out.println("Cell Value: " + myCell.getRawValue());
+                        break;   
+                    }
+                    System.out.println("---");
+ 
+                    if(myCell.getColumnIndex() == 0 && 
+                            !myCell.getStringCellValue().trim().equals("") &&
+                            !myCell.getStringCellValue().trim().equals("Item Number")){
+                        count++;
+                    }
+ 
+                }
+ 
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace(); 
+        }
+ 
+        return count;
+ 
+    }
+    
+}
